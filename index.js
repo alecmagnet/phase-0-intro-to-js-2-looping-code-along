b611f0c75069542d65c63b74072a414bc97c0ffe
Alec Magnet <amagnet@gmail.com>
2021-06-16 18:43:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const name = ["Lisa", "Kaitlin", "Jan"];
+const event = ["surprise"]
+
+function writeCards(name, event) {
+	const cardMessages = [];
+	for (let i = 0; i < name.length; i++) {
+		cardMessages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+	}
+	return cardMessages;
+}
+
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number--);
+	}
+}
