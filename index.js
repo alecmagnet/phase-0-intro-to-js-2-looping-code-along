d8ab89182709ff8f7de071defe895390c40d2361
Alec Magnet <amagnet@gmail.com>
2021-06-29 21:37:06 -0400

    commit tree 3b06597c4470544b1e40d0421348cdaa44ebeab0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solutions in this file
+/*
 const name = ["Lisa", "Kaitlin", "Jan"];
 const event = ["surprise"]
 
@@ -13,5 +14,40 @@ function writeCards(name, event) {
 function countDown(number) {
 	while (number >= 0) {
 		console.log(number--);
+	}
+}
+*/
+
+/*
+for (let age = 30; age < 40; age++) {
+	console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+	for (let i = 0; i < gifts.length; i++) {
+		console.log(`Wrapped ${gifts[i]} and added a bow!`);
+	}
+	return gifts;
+}
+wrapGifts(gifts);
+*/
+
+//const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+	const cards = [];
+	for (let i = 0; i < names.length; i++) {
+		cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+	}
+	return cards;
+}
+
+function countDown(positiveInteger) {
+	while (positiveInteger >= 0) {
+		console.log(positiveInteger--);
 	}
 }
